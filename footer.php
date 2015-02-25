a37d2b5af4e1713a290d17d8e5fe6350d222e8e4
Nishita Singhal <nishitasinghal@gmail.com>
2015-02-25 15:16:30 +0000

    Add Github link to footer. Adjust styling on p tag to fix line height. Change to nav to make more semantic.

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <footer>
 	<div class="width-960 padding">
 		<p class="col-half">&copy; Nishita Singhal 2015. Proudly handcoded.</p>
-		<p class="col-half social-links">
+		<nav class="col-half social-links">
 			<a href="mailto:hello@nishita.co.uk"><img src="img/email.svg" width="40" height="40" alt="Email"></a>
 			<a href="http://www.behance.net/nishitasinghal"><img src="img/behance.svg" width="40" height="40" alt="Behance"></a>
 			<a href="http://uk.linkedin.com/in/nishitasinghal"><img src="img/linkedin.svg" width="40" height="40" alt="LinkedIn"></a>
 			<a href="http://twitter.com/nishsinghal"><img src="img/twitter.svg" width="40" height="40" alt="Twitter"></a>
-		</p>
+			<a href="https://github.com/nishsinghal"><img src="img/github.svg" width="40" height="40" alt="Github"></a>
+		</nav>
 	</div>
 </footer>
 <script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
