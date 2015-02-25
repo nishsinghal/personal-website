8c306212f6c3bee73741a9411241d47a0be1b96d
Nishita Singhal <nishitasinghal@gmail.com>
2015-02-25 16:38:23 +0000

    Remove unneeded prefixes and adjust footer links

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 		<p class="col-half">&copy; Nishita Singhal 2015. Proudly handcoded.</p>
 		<nav class="col-half social-links">
 			<a href="mailto:hello@nishita.co.uk"><img src="img/email.svg" width="40" height="40" alt="Email"></a>
-			<a href="http://www.behance.net/nishitasinghal"><img src="img/behance.svg" width="40" height="40" alt="Behance"></a>
+			<a href="https://www.behance.net/nishitasinghal"><img src="img/behance.svg" width="40" height="40" alt="Behance"></a>
 			<a href="http://uk.linkedin.com/in/nishitasinghal"><img src="img/linkedin.svg" width="40" height="40" alt="LinkedIn"></a>
-			<a href="http://twitter.com/nishsinghal"><img src="img/twitter.svg" width="40" height="40" alt="Twitter"></a>
+			<a href="https://twitter.com/nishsinghal"><img src="img/twitter.svg" width="40" height="40" alt="Twitter"></a>
 			<a href="https://github.com/nishsinghal"><img src="img/github.svg" width="40" height="40" alt="Github"></a>
 		</nav>
 	</div>
